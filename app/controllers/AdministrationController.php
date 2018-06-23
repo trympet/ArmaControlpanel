ea5cbbd4955849b9d6287716f79b8ba2ec0d9ad4
trym <trym2001@hotmail.com>
2018-06-24 00:40:58 +0200

    test

--- app/controllers/AdministrationController.php
+++ app/controllers/AdministrationController.php
@@ -55,7 +55,7 @@ class AdministrationController extends BaseController
             return Redirect::to('backend#backend/dashboard/index');
         
         $data['profile'] = Auth::user();
-        $data['armapath'] = DB::select('select armapath from parameters');
+        $data['armapath'] = DB::select('SELECT armapath FROM parameters');
 
         return View::make('backend.administration.parameters', $data);
     }
